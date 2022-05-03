5ec4e38c0f9c0706b824ce5cda09be44cbdc3db5
mounkaila <mounkaila144@gmail.com>
2022-05-03 16:36:54 +0100

    init

---
+++ src/Controller/FileController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FileController extends AbstractController
+{
+    #[Route('/image/{img}', name: 'image')]
+    public function index($img): Response
+    {
+        //Récupération du dossier racine grace au kernel et ensuite ajout de l'emplacement du
+        //fichier
+        $filename = $this->getParameter('kernel.project_dir') . '/public/images/products/' . $img;
+        //Si le fichier existe alors on le renvoi, sinon retour 404
+        if (file_exists($filename)) {
+            //retour d'un new BinaryFileResponse avec le nom du fichier
+            return new BinaryFileResponse($filename);
+        } else {
+            return new JsonResponse(null, 404);
+        }
+    }
+
+}
---
+++ src/Entity/Pub.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\PubRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+#[ORM\Entity(repositoryClass: PubRepository::class)]
+#[Vich\Uploadable]
+#[ApiResource]
+class Pub
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $description;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $nom;
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     */
+    #[Vich\UploadableField(mapping: 'product_image', fileNameProperty: 'videoName')]
+    private ?File $videoFile = null;
+
+    #[ORM\Column(type: 'string')]
+    private ?string $videoName = null;
+
+    #[ORM\Column(type: 'datetime')]
+    private ?\DateTimeInterface $updatedAt = null;
+
+    /**
+     * @return \DateTimeInterface|null
+     */
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $videoFile
+     */
+    public function setVideoFile(?File $videoFile = null): void
+    {
+        $this->videoFile = $videoFile;
+
+        if (null !== $videoFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+
+
+    public function getVideoFile(): ?File
+    {
+        return $this->videoFile;
+    }
+
+    public function setVideoName(?string $videoName): void
+    {
+        $this->videoName = $videoName;
+    }
+
+    public function getVideoName(): ?string
+    {
+        return $this->videoName;
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+}
